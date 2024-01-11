5f8c1f449b8b73faa5ed804ab00633a7d23cf53b
Priyanshu Singh <priyan8605@gmail.com>
2024-01-12 03:27:18 +0530

    completed the project

--- top_course/src/App.js
+++ top_course/src/App.js
@@ -3,75 +3,73 @@ import './App.css'
 import Navbar from "./Components/Navbar"
 import Filter from "./Components/Filter"
 import Cards from "./Components/Cards"
-import {apiUrl,filterData} from "./data"//{filterData} pass krenge <filter filterData={filterData} as props/>
+import { apiUrl, filterData } from "./data"//{filterData} pass krenge <filter filterData={filterData} as props/>
 import Spinner from "./Components/Spinner"
-import {ToastContainer} from "react-toastify"
+import { ToastContainer } from "react-toastify"
 import 'react-toastify/dist/ReactToastify.css'//css file of toast
-import {toast} from 'react-toastify'
+import { toast } from 'react-toastify'
 
 function App() {
   // starting me hum saare courses ke card dikhayenge mtlb "All" category ke card dikhayenge
   //jb web page load hoga for the first time
-  const [category,setCategory]=useState(filterData[0].title);//filterData array ke 0th index ke 
+  const [category, setCategory] = useState(filterData[0].title);//filterData array ke 0th index ke 
   // object pe jo title property hai (i.e. title:"All") vo by  default category ke andr store hoga
   // iske baad setCategory ko pass kro as props in Filter component
   // "category" is an  array as it's defined in useState and useState returns an "array"
 
-  const [loading,setLoading]=useState(true)//for buffering jbb tkk load nhi hota
-  const [courses,setCourses]=useState(null)
+  const [loading, setLoading] = useState(true)//for buffering jbb tkk load nhi hota
+  const [courses, setCourses] = useState(null)
   // hmare pss sare "Card" ka data pda hai hmare "apiUrl" me isliye hum APi call krenge in APP.js
   // abb "App.js" se "Cards" component me hum data pass krenge using props
   //hum API call krenge using UseEffect=> agr component render hone ke baad hume koi bhi task perform krna hai jaise ki api call krna to hum 
   // use krenge useEffect() ko
 
-  
-  const fetchData=async()=>{
+
+  const fetchData = async () => {
 
     // agr humne webpage khola aur abhi tkk data nhi aaya hai url se or ya to jbb tkk url se aaye hue data ka processing chal rha hai hoga tbb tk hum buffering show krenge
-  //jaise url se data aajayga buffering show hona bnd krr denge
-  setLoading(true);//jbb tkk url se data nhi aaya hai tbb tkk true rhega mtlb "spinner" dikhega
-    try{
-      const res=await fetch(apiUrl);//jo bi data laayga apiUrl that will be stored in "res" 
-      const output=await res.json();//jo bhi "res" ke andr data hai that will be converted into json format 
+    //jaise url se data aajayga buffering show hona bnd krr denge
+    setLoading(true);//jbb tkk url se data nhi aaya hai tbb tkk true rhega mtlb "spinner" dikhega
+    try {
+      const res = await fetch(apiUrl);//jo bi data laayga apiUrl that will be stored in "res" 
+      const output = await res.json();//jo bhi "res" ke andr data hai that will be converted into json format 
       // bcoz of res.json() and that json format data will be stored in "output" variable
-      
+
       console.log(output);
       //save data in a variable
       setCourses(output.data);//"output" variable ke andr jo data(key) hai that will get stored in courses
       // abb "courses" ke andr "output" ka data aa gya so we will pass it into <Cards courses={courses}/>
     }
-    catch(error)
-    {
+    catch (error) {
       toast.error(error);
     }
     setLoading(false);//jaise url se data aa jayga to setLoading ko hum false krr denge mtlb "spinner" not shown
   }
-  useEffect(()=>
-  {
-  fetchData();//
-},[]);//mounting ke time bss render hoga ooske baad nhi
+  useEffect(() => {
+    fetchData();//
+  }, []);//mounting ke time bss render hoga ooske baad nhi
 
   return (
-  <div className="min-h-screen  flex flex-col">
+    <div className="min-h-screen  flex flex-col bg-blue-400">
       <div>
-      <Navbar/>
+        <Navbar />
       </div>
       <div className='bg-blue-400'>
-      <div>
-      <Filter setCategory={setCategory}
-      filterData={filterData}
-      />
-      </div>
-      <div className="w-11/12 max-w-[1200px] min-h-[50vh] mx-auto flex flex-wrap justify-center items-center">
-      {/* agr "loading" ka value true hai then "spinner" will be seen and if "loading" value
+        <div>
+          <Filter setCategory={setCategory}
+            filterData={filterData}
+          />
+        </div>
+        <div className="w-11/12 max-w-[1200px] min-h-[50vh] mx-auto flex flex-wrap justify-center items-center">
+          {/* agr "loading" ka value true hai then "spinner" will be seen and if "loading" value
       is false then spinner won't be seen instead cards will be seen*/}
-     { loading?(<Spinner/>):(<Cards courses={courses} category={category}/>)}
-      </div>
-    </div >
+          {loading ? (<Spinner />) : (<Cards courses={courses} category={category} />)}
+        </div>
+      </div >
       <div>
-        <ToastContainer/>
-        </div>                                                                                            
-  </div>
+        <ToastContainer />
+      </div>
+    </div>
   )
 }
 
--- top_course/src/Components/Filter.js
+++ top_course/src/Components/Filter.js
@@ -1,37 +1,78 @@
 import React from "react";
 
 // {filterData}
-const Filter=({filterData,category,setCategory})=>{
+const Filter = ({ filterData, category, setCategory }) => {
 
-     function filterHandler(title){
+    function filterHandler(title) {
         // abb hum chate hai ki Development prr click kre to development ke card show ho 
         //buisneess prr click kre to buisness  ke card show ho  
         // hmare pass to card ka data hai nhi sirf category ,setCategory and
         //filterData hai
-       // jaise hum click krenge kisi filter (i.e  all ,development,etc) prr ooska title ooska title hum store
-       //kralenge and hum send krr denge fileHandler me oclickHandler ke through
-       setCategory(title);
-     }
+        // jaise hum click krenge kisi filter (i.e  all ,development,etc) prr ooska title ooska title hum store
+        //kralenge and hum send krr denge fileHandler me oclickHandler ke through
+        setCategory(title);
+    }
 
-    // jaise Filter=({filterData}) aa jayega hum 5 buttons bna skte hai based on that {filterData
+   // jaise Filter=({filterData}) aa jayega hum 5 buttons bna skte hai based on that {filterData
     return (<div className="w-11/12 flex flex-wrap max-w-max space-x-4 mx-auto gap-y-4 py-4 justify-center" >
-         {filterData.map( (data) => {
-        // harr ekk filter data ke liye ekk button bna do aur harr ek button me title daal do
-        return( <button className={`text-lg px-2 py-1 rounded-md font-medium
-         text-white bg-black border-2 hover:bg-opacity-50 transition-all duration-200`}
-        key={data.id}
-        //jaise hum button prr click krenge different type of card show hoga based on data ke title ya category pr
-        //mtlb button pr evetListener lga hai
-        onClick={()=>{
-            return filterHandler(data.title)//data ka title pass krr rhe hai
-        }}
-        >
-            {data.title}
-        </button>
-        )
-         })
+        {
+         filterData.map((data) => {
+            // harr ekk filter data ke liye ekk button bna do aur harr ek button me title daal do
+          return( <button className={`text-lg px-2 py-1 rounded-md font-medium
+         text-white bg-black  hover:bg-opacity-50 transition-all duration-400
+         ${(category )=== (data.title )? (`bg-opacity-70 border-white`) : (`bg-opacity-40 border-transparent`)}
+         `}
+
+                //agr category ke andr ka value data object ke "title" ke brabar hai to opacity=60
+                //and border=white and agr aisa nhi hai to border=transparent and opacity=40
+
+                key={data.id}
+                //jaise hum button prr click krenge different type of card show hoga based on data ke title ya category pr
+                //mtlb button pr evetListener lga hai
+                onClick={() => {
+                    return filterHandler(data.title)//data ka title pass krr rhe hai
+                }
+            }
+            >
+                {data.title}
+            </button>
+          ) 
+        })
         }
-        </div>
+    </div>
     )
+
+    // return (
+    //     <div className="w-11/12 flex flex-wrap max-w-max space-x-4 mx-auto gap-y-4 py-4 justify-center">
+    //       {filterData.map((data) => {
+    //     if(category===data.title){
+    //         return (
+    //           <button className="text-lg px-2 py-1 rounded-md font-medium text-white bg-black border-2 hover:bg-opacity-50 transition-all duration-200
+    //               bg-opacity-60 border-white"
+                
+    //             key={data.id}
+    //             onClick={() => filterHandler(data.title)}
+    //           >
+    //             {data.title}
+    //           </button>
+    //         );
+    //       }
+    //       else{
+    //         return (
+    //             <button
+    //               className="text-lg px-2 py-1 rounded-md font-medium text-white bg-black border-2 hover:bg-opacity-50 transition-all duration-200
+    //                 bg-opacity-60 border-transparent " 
+                  
+    //               key={data.id}
+    //               onClick={() => filterHandler(data.title)}
+    //             >
+    //               {data.title}
+    //             </button>
+    //         ) 
+    //            }
+    //         })
+    //       }     
+    //      </div>
+    //       )
 }
 export default Filter;
